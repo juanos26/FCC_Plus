075f194d53a8396fcf90083ee39509656b94c5ab
juanos26 <113743113+juanos26@users.noreply.github.com>
2023-08-15 15:33:37 -0400

    2nd commit. Added almost all classes in their respective packages/folders. Added Chrome, Firefox, and general Selenium Libraires as well as a web driver manager to make it all work together. Tested main class to see if librairies work. Chrome driver is having issues but Firefox works fine. Started adding to the CurrentDevice Class.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 package src;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import src.Model.Student;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.youtube.com/");
+
 
     }
 }
---
+++ src/Model/CurrentDevice.java
@@ -0,0 +1,15 @@
+package src.Model;
+
+public class CurrentDevice {
+    private String OS;
+    private int resolutionWide;
+    private int resolutionHeight;
+    private String chosenBrowser;
+    private boolean darkMode = false;
+
+
+
+    CurrentDevice(){
+
+    }
+}
